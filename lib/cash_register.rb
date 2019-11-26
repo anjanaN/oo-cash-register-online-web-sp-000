e9c2d7fa451a84e79302fe799da2e986317d5905
Anjana Nair <anju740@gmail.com>
2019-11-26 02:58:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount = nil)
+    self.total = 0
+    self.discount = discount
+  end
+
+  def total
+    self.total
+  end
+end
